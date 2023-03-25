6d9c2700e4d6c7703c3408a13a99325cbbb01668
david9morais <david_morais@hotmail.com>
2023-03-25 23:19:15 +0000

    add method findAll to return all people mocked

--- src/main/java/com/example/demo/services/PersonService.java
+++ src/main/java/com/example/demo/services/PersonService.java
@@ -3,6 +3,8 @@ package com.example.demo.services;
 import com.example.demo.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -19,6 +21,27 @@ public class PersonService {
         person.setFirstName("David");
         person.setLastName("Morais");
         person.setAddress("Guimarães");
+        person.setGender("Male");
+        return person;
+    }
+
+    public List<Person> findAll() {
+        logger.info("Finding all people!");
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name "+ i);
+        person.setLastName("Last name "+ i);
+        person.setAddress("Some address in Portugal" + i);
         person.setGender("Male");
         return person;
     }
