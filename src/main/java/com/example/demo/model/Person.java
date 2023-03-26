8c31a7a2a824c4712070806b6d3e85bb59a994b6
david9morais <david_morais@hotmail.com>
2023-03-26 23:29:06 +0100

    create person repository and change class Person to entity

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -1,17 +1,30 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 762422831270511114L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
+
     private String address;
+
     private String gender;
 
     public Person() {
